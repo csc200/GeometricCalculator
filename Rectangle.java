2def3d0f522bd6fde71e5552f99c4b7a970ce950
Tanes Kanchanawanchai <tkanchanawanchai@nvcc.edu>
2017-10-03 17:00:11 -0400

    add input dialog with javafx

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,10 @@
 public class Rectangle {
-
+	@Override
+	public void start(Stage primaryStage) {
+		TextInputDialog textInputDialog = new TextInputDialog("Geometric Calculaor");
+		Optional<String> result = textInputDialog.showAndWait();
+	}
+	public static void main(String [] args) {
+		launch(args);
+	}
 }
