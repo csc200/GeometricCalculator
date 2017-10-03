65c4bde064cf748aa9fd18f7c3e24107b54d1ece
Tanes Kanchanawanchai <tkanchanawanchai@nvcc.edu>
2017-10-03 17:12:17 -0400

    Add parser for width input to double

--- Rectangle.java
+++ Rectangle.java
@@ -3,7 +3,10 @@ public class Rectangle {
 	public void start(Stage primaryStage) {
 		TextInputDialog textInputDialog = new TextInputDialog("Geometric Calculaor");
 		Optional<String> result = textInputDialog.showAndWait();
+		String widthInput = result.get();
+		double width = Doulbe.parseDouble(input);
 	}
+
 	public static void main(String [] args) {
 		launch(args);
 	}
